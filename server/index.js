7ec9c4cf50dc3748b8f91648c47cc855d755cdf0
intansuhana <syana2631@gmail.com>
2024-12-12 20:26:36 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -1,23 +1,55 @@
-import express from 'express'
+import express from 'express';
 import "dotenv/config";
-import {db} from "./config/db.js"
+import { db } from "./config/db.js";
+import userRouter from './routes/user.routes.js'; 
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;  
+
+app.use(
+    cors({
+        
+    origin:process.env.CLIENT_URL,
+    credentials:true,
+    })
+)
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/v1/auth',authRouter);
+
+
+
+app.use((req, res, next) => {
+    console.log(`[${req.method}] ${req.url}`);
+    next();
+});
+
 
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "hello world"
-    })
-})
+    });
+});
+
+
+app.use('/api/v1/users', (req, res, next) => {
+    console.log('[DEBUG] Request masuk ke /api/v1/users');
+    next();
+}, userRouter);
+
 
 app.use("*", (req, res) => {
     res.status(404).json({
         message: "not found"
-    })
-})
+    });
+});
+
 
 app.listen(PORT, () => {
-    console.log('server strated, list on port ${PORT}');
-})
-// test
+    console.log(`Server started, listening on port ${PORT}`);
+});
